3492c7a4d1dcb55289c9f1191be7ed32995fa006
Zafer Balkan <zafer@zaferbalkan.com>
2022-02-23 20:27:24 +0200

    Used Chocolatey.lib for a minor performance improvement

--- source/UninstallTools/Factory/ChocolateyFactory.cs
+++ source/UninstallTools/Factory/ChocolateyFactory.cs
@@ -1,204 +1,151 @@
-﻿using Klocman.Tools;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Diagnostics.CodeAnalysis;
+using chocolatey;
+using chocolatey.infrastructure.app.domain;
+using chocolatey.infrastructure.results;
+using Klocman.Extensions;
+using Klocman.IO;
+using Klocman.Tools;
+using UninstallTools.Junk.Containers;
+using UninstallTools.Junk.Confidence;
 using UninstallTools.Properties;
 
 namespace UninstallTools.Factory
 {
     public sealed class ChocolateyFactory : IIndependantUninstallerFactory
     {
-        private static bool? _chocoIsAvailable;
-        private static string _chocoLocation;
+        private static GetChocolatey choco;
+        private static bool? chocoIsAvailable;
+        private static string chocoFullFilename;
 
-        private static string ChocoFullFilename
-        {
-            get
-            {
-                if (_chocoLocation == null)
-                    GetChocoInfo();
-                return _chocoLocation;
-            }
-        }
-
-        private static bool ChocoIsAvailable
-        {
-            get
-            {
-                if (!_chocoIsAvailable.HasValue)
-                {
-                    _chocoIsAvailable = false;
-                    GetChocoInfo();
-                }
-                return _chocoIsAvailable.Value;
-            }
-        }
-
-        private static void GetChocoInfo()
-        {
-            try
-            {
-                var chocoPath = PathTools.GetFullPathOfExecutable("choco.exe");
-                if (string.IsNullOrEmpty(chocoPath)) return;
-
-                var result = StartProcessAndReadOutput(chocoPath, string.Empty);
-                if (result.StartsWith("Chocolatey", StringComparison.Ordinal))
-                {
-                    _chocoLocation = chocoPath;
-                    _chocoIsAvailable = true;
-                }
-            }
-            catch (SystemException ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-
-        private static readonly string[] NewlineSeparators = StringTools.NewLineChars.ToArray();
-
-        public IList<ApplicationUninstallerEntry> GetUninstallerEntries(ListGenerationProgress.ListGenerationCallback progressCallback)
+        public IList<ApplicationUninstallerEntry> GetUninstallerEntries(ListGenerationProgress.ListGenerationCallback progressCallback = null)
         {
             var results = new List<ApplicationUninstallerEntry>();
+            if (!IsChocoAvailable()) return results;
 
-            if (!ChocoIsAvailable) return results;
+            InitiateChocolatey();
+            var sw = Stopwatch.StartNew();
+            var packages = choco.List<PackageResult>();
+            Console.WriteLine($"[Performance] Retrieving package list took {sw.ElapsedMilliseconds}ms");
 
-            var result = StartProcessAndReadOutput(ChocoFullFilename, @"list -lo -nocolor --detail");
-
-            if (string.IsNullOrEmpty(result)) return results;
-
-            var re = new System.Text.RegularExpressions.Regex(@"\n\w.+\r\n Title:");
-            var match = re.Match(result);
-            if (!match.Success) return results;
-            var begin = match.Index + 1;
-
-            while(true)
-            {
-                match = match.NextMatch();
-                if (!match.Success) break;
-                var end = match.Index + 1;
-                var info = result.Substring(begin, end-begin);
-                int i = info.IndexOf(' '), j = info.IndexOf("\r\n");
-                var appName = new { name = info.Substring(0, i), version = info.Substring(i+1, j-i-1) };
-
-                var kvps = ExtractPackageInformation(info);
-                if (kvps.Count == 0) continue;
-
-                var entry = new ApplicationUninstallerEntry();
-
-                AddInfo(entry, kvps, "Title", (e, s) => e.RawDisplayName = s);
-
-                entry.DisplayVersion = ApplicationEntryTools.CleanupDisplayVersion(appName.version);
-                entry.RatingId = "Choco " + appName.name;
-                entry.UninstallerKind = UninstallerType.Chocolatey;
-
-                AddInfo(entry, kvps, "Summary", (e, s) => e.Comment = s);
-                if (string.IsNullOrEmpty(entry.Comment))
-                {
-                    AddInfo(entry, kvps, "Description", (e, s) => e.Comment = s);
-                    if (string.IsNullOrEmpty(entry.Comment))
-                        AddInfo(entry, kvps, "Tags", (e, s) => e.Comment = s);
-                }
-
-                AddInfo(entry, kvps, "Documentation", (e, s) => e.AboutUrl = s);
-                if (string.IsNullOrEmpty(entry.AboutUrl))
-                {
-                    AddInfo(entry, kvps, "Software Site", (e, s) => e.AboutUrl = s);
-                    if (string.IsNullOrEmpty(entry.AboutUrl))
-                        AddInfo(entry, kvps, "Chocolatey Package Source", (e, s) => e.AboutUrl = s);
-                }
-
-                var psc = new ProcessStartCommand(ChocoFullFilename, $"uninstall {appName.name} -y -r");
-
-                entry.UninstallString = psc.ToString();
-
-                if (entry.RawDisplayName == "Chocolatey")
-                    entry.InstallLocation = GetChocoInstallLocation();
-
-                // Prevent chocolatey from trying to run the original uninstaller (it's deleted by now), only remove the package
-                psc.Arguments += " -n --skipautouninstaller";
-                var junk = new Junk.Containers.RunProcessJunk(entry, null, psc, Localisation.ChocolateyFactory_UninstallInChocolateyJunkName);
-                junk.Confidence.Add(Junk.Confidence.ConfidenceRecords.ExplicitConnection);
-                junk.Confidence.Add(4);
-                entry.AdditionalJunk.Add(junk);
-
-                results.Add(entry);
-                begin = end;
-            }
+            sw.Restart();
+            packages.AsParallel().AsOrdered().ForEach(package => results.Add(ConvertToEntry(package)));
+            Console.WriteLine($"[Performance] PARALLEL: Mapping PackageResult to Application for entry took {sw.ElapsedMilliseconds}ms");
 
             return results;
-        }
-
-        private static string GetChocoInstallLocation()
-        {
-            // The path is C:\ProgramData\chocolatey\bin\choco.exe OR C:\ProgramData\chocolatey\choco.exe
-            var chocoLocation = Path.GetDirectoryName(ChocoFullFilename);
-            if (chocoLocation != null && chocoLocation.EndsWith(@"\bin", StringComparison.OrdinalIgnoreCase))
-                return chocoLocation.Substring(0, chocoLocation.Length - 4);
-            return chocoLocation;
-        }
-
-        private static void AddInfo(ApplicationUninstallerEntry target, Dictionary<string, string> source,
-            string key, Action<ApplicationUninstallerEntry, string> setter)
-        {
-            if (source.TryGetValue(key, out var val))
-            {
-                try
-                {
-                    setter(target, val);
-                }
-                catch (SystemException ex)
-                {
-                    Console.WriteLine(@"Exception while extracting info from choco: " + ex.Message);
-                }
-            }
-        }
-
-        private static Dictionary<string, string> ExtractPackageInformation(string result)
-        {
-            // Parse the console output into lines, then into key-value pairs
-            var lines = result.Split(NewlineSeparators, StringSplitOptions.RemoveEmptyEntries)
-                .Where(x => x.Length > 2 && x[0] == ' ' && x[1] != ' ' && x.Contains(": "))
-                .Select(x => x.TrimStart())
-                .SelectMany(x => x.Split(new[] { " | " }, StringSplitOptions.None));
-
-            var kvps = new Dictionary<string, string>();
-            foreach (var line in lines)
-            {
-                var i = line.IndexOf(": ", StringComparison.Ordinal);
-                if (i <= 0) continue;
-
-                var key = line.Substring(0, i);
-                var val = line.Substring(i + 2);
-                if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(val)) continue;
-
-                kvps.Add(key, val);
-            }
-
-            return kvps;
         }
 
         public bool IsEnabled() => UninstallToolsGlobalConfig.ScanChocolatey;
         public string DisplayName => Localisation.Progress_AppStores_Chocolatey;
 
-        private static string StartProcessAndReadOutput(string filename, string args)
+        private static void InitiateChocolatey()
         {
-            using (var process = Process.Start(new ProcessStartInfo(filename, args)
+            if (choco == null)
             {
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = false,
-                CreateNoWindow = true,
-                StandardOutputEncoding = Encoding.Default
-            }))
-            {
-                var sw = Stopwatch.StartNew();
-                var output = process?.StandardOutput.ReadToEnd();
-                Console.WriteLine($"[Performance] Running command {filename} {args} took {sw.ElapsedMilliseconds}ms");
-                return output;
+                choco = Lets.GetChocolatey(initializeLogging: false);
+                choco.Set(
+                    config =>
+                    {
+                        config.CommandName = nameof(CommandNameType.list);
+                        config.ListCommand.LocalOnly = true;
+                    });
             }
+        }
+
+        private static ApplicationUninstallerEntry ConvertToEntry(PackageResult package)
+        {
+            var entry = new ApplicationUninstallerEntry
+            {
+                DisplayName = SanitizeString(package.Package.Title),
+                DisplayVersion = ApplicationEntryTools.CleanupDisplayVersion(package.Version),
+                Comment = SanitizeString(package.Package.Summary ?? package.Package.Description.Split('\n')[0] ?? package.Package.Tags),
+                AboutUrl = package.Package.DocsUrl?.AbsoluteUri ?? package.Package.ProjectUrl?.AbsoluteUri,
+                InstallLocation = package.InstallLocation,
+                DisplayIcon = string.Empty,
+                EstimatedSize = FileSize.Empty,
+                IconBitmap = null,
+                InstallDate = DateTime.MinValue,
+                RatingId = $"Choco {package.Package.Id}",
+                UninstallerKind = UninstallerType.Chocolatey
+            };
+
+            // A special occasion that needs to be handled.
+            if (package.Package.Title.Equals("Chocolatey"))
+            {
+                entry.InstallLocation = Environment.GetEnvironmentVariable("ChocolateyInstall");
+            }
+
+            var psc = new ProcessStartCommand(chocoFullFilename, $"uninstall {package.Package.Id} -y -r");
+            entry.UninstallString = psc.ToString();
+
+            // Prevent chocolatey from trying to run the original uninstaller (it's deleted by now), only remove the package
+            psc.Arguments += " -n --skipautouninstaller";
+            var junk = new RunProcessJunk(entry, null, psc, Localisation.ChocolateyFactory_UninstallInChocolateyJunkName);
+            junk.Confidence.Add(ConfidenceRecords.ExplicitConnection);
+            junk.Confidence.Add(4);
+            entry.AdditionalJunk.Add(junk);
+            return entry;
+        }
+
+        private static bool IsChocoAvailable()
+        {
+            if (!chocoIsAvailable.HasValue)
+            {
+                GetChocoInfo();
+            }
+
+            return chocoIsAvailable.Value;
+        }
+        private static void GetChocoInfo()
+        {
+            var chocoPath = string.Empty;
+            if (string.IsNullOrWhiteSpace(chocoFullFilename))
+            {
+                var pathVar = Environment.GetEnvironmentVariable("PATH");
+                if (!string.IsNullOrWhiteSpace(pathVar))
+                {
+                    chocoPath =
+                        pathVar.Split(';')
+                            .SingleOrDefault(
+                                path => path.IndexOf("Chocolatey", StringComparison.OrdinalIgnoreCase) > -1);
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(chocoPath))
+            {
+                chocoPath = string.Empty;
+            }
+            var exePath = Path.Combine(chocoPath, "choco.exe");
+            if (File.Exists(exePath))
+            {
+                chocoIsAvailable = true;
+                chocoFullFilename = exePath;
+            }
+        }
+
+        // Chocolatey CLI probably handles this before printing, that's why we do not need this code while parsing
+        private static string SanitizeString(string text)
+        {
+            if (text.IndexOf('\u2013') > -1) text = text.Replace('\u2013', '-'); // en dash
+            if (text.IndexOf('\u2014') > -1) text = text.Replace('\u2014', '-'); // em dash
+            if (text.IndexOf('\u2015') > -1) text = text.Replace('\u2015', '-'); // horizontal bar
+            if (text.IndexOf('\u2017') > -1) text = text.Replace('\u2017', '_'); // double low line
+            if (text.IndexOf('\u2018') > -1) text = text.Replace('\u2018', '\''); // left single quotation mark
+            if (text.IndexOf('\u2019') > -1) text = text.Replace('\u2019', '\''); // right single quotation mark
+            if (text.IndexOf('\u201a') > -1) text = text.Replace('\u201a', ','); // single low-9 quotation mark
+            if (text.IndexOf('\u201b') > -1) text = text.Replace('\u201b', '\''); // single high-reversed-9 quotation mark
+            if (text.IndexOf('\u201c') > -1) text = text.Replace('\u201c', '\"'); // left double quotation mark
+            if (text.IndexOf('\u201d') > -1) text = text.Replace('\u201d', '\"'); // right double quotation mark
+            if (text.IndexOf('\u201e') > -1) text = text.Replace('\u201e', '\"'); // double low-9 quotation mark
+            if (text.IndexOf('\u2026') > -1) text = text.Replace("\u2026", "..."); // horizontal ellipsis
+            if (text.IndexOf('\u2032') > -1) text = text.Replace('\u2032', '\''); // prime
+            if (text.IndexOf('\u2033') > -1) text = text.Replace('\u2033', '\"'); // double prime
+            if (text.IndexOf('\u00AE') > -1) text = text.Replace('\u00AE', '©'); // register mark
+            return text;
         }
     }
 }
